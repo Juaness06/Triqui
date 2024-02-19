f1fea021bfd092fe6a1385ef4740d0e0c33fb292
Juan Esteban Granada O'Brien <134427814+Juaness06@users.noreply.github.com>
2024-02-19 08:22:08 -0500

    Subir archivos

---
+++ Codigo/Triqui/constantes.py
@@ -0,0 +1,27 @@
+ANCHO = 600
+ALTURA = 600
+
+FILAS = 3
+COLUMNAS = 3
+
+# CUADRADOS
+TAMANO_CUADRO = ANCHO // COLUMNAS
+
+RADIO_CIRCULO = TAMANO_CUADRO // 4
+
+# Tamaño de las figuras
+LINEA_ANCHO = 15
+ANCHO_CIRCULO = 15
+ANCHO_X = 20
+
+# Colores del tablero
+FONDO_COLOR = (97, 62, 12)
+
+# Lineas
+LINEA_COLOR = (0, 0, 0)
+
+# Color del circulo y la cruz
+COLOR_CIRCULO = (100, 100, 100)
+COLOR_X = (12, 97, 62)
+
+OFFSET = 50
